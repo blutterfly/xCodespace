1dcff2ac59cc9053a71616c840db16ab76b1b32b
blutterfly <larry.prestsoa@gmail.com>
2024-09-13 11:44:04 +0000

    todo app works.

--- app.py
+++ app.py
@@ -1,11 +1,80 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Needed for flashing messages
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+# Define the Task model for the database
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task_name = request.form.get('task')
+    if not task_name:
+        flash('Task name cannot be empty!', 'error')
+        return redirect(url_for('index'))
+
+    new_task = Task(name=task_name)
+    db.session.add(new_task)
+    db.session.commit()
+    flash('Task added successfully!', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:task_id>')
+def complete(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        task.done = True
+        db.session.commit()
+        flash('Task marked as complete!', 'success')
+    else:
+        flash('Task not found!', 'error')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        flash('Task deleted successfully!', 'success')
+    else:
+        flash('Task not found!', 'error')
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        flash('Task not found!', 'error')
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        task_name = request.form.get('task')
+        if not task_name:
+            flash('Task name cannot be empty!', 'error')
+            return redirect(url_for('edit', task_id=task_id))
+
+        task.name = task_name
+        db.session.commit()
+        flash('Task updated successfully!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', task=task)
+
+if __name__ == '__main__':
+    # Create the database and tables
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
